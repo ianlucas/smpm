fddc8bef1221639085cc3a00f0f224919cbf1e0a
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-04-27 00:37:32 -0300

    this was completly wrong

--- sourcemod/process.py
+++ sourcemod/process.py
@@ -4,10 +4,11 @@ import subprocess
 def exec(command: str, input: list[str]):
     buffer = ""
     proc = subprocess.Popen(
-        command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command + " ".join(input),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
     )
-    for line in input:
-        proc.stdin.write(line.encode("utf-8"))
     stdout, stderr = proc.communicate()
     buffer = stdout.decode("utf-8") + stderr.decode("utf-8")
     code = proc.returncode
