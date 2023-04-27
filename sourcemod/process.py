55d692627b599504250edbaf5469422b53513e32
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-04-27 00:44:55 -0300

    ok lets not overcomplicate it

--- sourcemod/process.py
+++ sourcemod/process.py
@@ -2,15 +2,7 @@ import subprocess
 
 
 def exec(command: str, input: list[str]):
-    buffer = ""
     arguments = " ".join(input)
-    proc = subprocess.Popen(
-        f"{command} {arguments}",
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    stdout, stderr = proc.communicate()
-    buffer = stdout.decode("utf-8") + stderr.decode("utf-8")
-    code = proc.returncode
-    return {"buffer": buffer, "code": code}
+    fullcommand = f"{command} {arguments}"
+    process = subprocess.run(fullcommand, shell=True, capture_output=True)
+    return {"code": process.returncode, "buffer": str(process.stdout)}
